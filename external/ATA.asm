d7fe248be03306d9c6290d71946ae48b520e8e79
Christos Themelis <themelisx@gmail.com>
2022-06-13 00:38:26 +0300

    Update ATA.asm

--- external/ATA.asm
+++ external/ATA.asm
@@ -1,5 +1,7 @@
 ;example code for executables
 
+%define DEBUG
+
 [BITS 32]
 
 org 0
@@ -35,9 +37,11 @@ Print:			dd	?
 			db	'PrintC', 0
 PrintC:			dd	?
 			db	'PrintCRLF', 0
-PrintCRLF:		dd	?
+PrintCRLF:		dd	?			
 			db	'PrintHex', 0
 PrintHex:		dd	?
+			db	'PrintHexView', 0
+PrintHexView:		dd	?
 			db	'MemAlloc', 0
 MemAlloc		dd	?
 			db	'MemFree', 0
@@ -48,6 +52,7 @@ ExportTable:
 
 ;data
 
+mySector dd ?
 command			db	0
 drive			db	0
 port			dw	0
@@ -73,7 +78,7 @@ FixAddress:
 	sub	eax, .local
 	ret
 
-
+align 4
 ;entry point here
 ExecutableEIP:
 	push	ebp
@@ -84,12 +89,13 @@ ExecutableEIP:
 	
 	pop	ebp
 	ret
+
   	
 ;Gets the Ata/Atapi drive info 
 
 ;ATA_Identify_Device command = 0xec
 ;ATA_Identify_Packet_Device = 0xa1
-
+align 4
 ATA_ShowInfo:	
 	pushad
 
@@ -97,7 +103,7 @@ ATA_ShowInfo:
 	push	eax
 	call	[ebp + PrintHex]
 	
-        movzx	eax, byte [ebp + drive]
+  movzx	eax, byte [ebp + drive]
 	push	eax
 	call	[ebp + PrintHex]
 	
@@ -107,12 +113,146 @@ ATA_ShowInfo:
 	push	dword 28h
 	call	[ebp + PrintC]
 	call	[ebp + PrintCRLF]
+
+	mov		dword [ebp + mySector], 1
+.read1:
+	call	ReadATASector
 	
+	push	dword [ebp + ATA_Buffer]
+  push	dword 256
+  call	[ebp + PrintHexView]
+
+	mov		eax, dword [ebp + mySector]  
+	inc		eax
+	mov		dword [ebp + mySector], eax
+	;cmp		eax, 16
+	;jne		.read1
+	
+	;jmp $
+  
 	popad
 	ret
+	
+align 4
+ReadATASector:
+; parameters:
+;------------
+; eax = lba
+; edi = location where to put the data (512 bytes)
+; dl  = drive, 0 = master, 1 = slave
 
+	;calculate LBA
+	;LBA = ( ( CYL * HPC + HEAD ) * SPT ) + SECT - 1
+	;LBA: linear base address of the block
+	;CYL: value of the cylinder CHS coordinate
+	;HPC: number of heads per cylinder for the disk
+	;HEAD: value of the head CHS coordinate
+	;SPT: number of sectors per track for the disk
+	;SECT: value of the sector CHS coordinate
+	
+		;pushad
+	
+		mov		eax, dword [ebp + mySector]
+		mov		edi, dword [ebp + ATA_Buffer]
+		mov		dl, 1
+	
+		test 	eax, 0xF0000000	; test for bits 28-31
+  	jnz	  short .return_error
+  	test 	dl, 0xFE		; test for invalid device ids
+  	jnz 	short .return_error
+  
+  	call 	ATA_WaitNotBusy
+  	
+  	mov 	edx, dword [ebp + port]
+  	add		edx, 2		;sector count register
+ 		mov 	al, 1			;read one sector at any given time
+  	out 	dx, al
+  	
+  	;DriveLBA = (((M * 60) + S) * 75 + F) -150. Where M is minutes, S is seconds, and F is frames.
+  	
+  	;mov		ecx, 16		; LBA ***********************************************
+  	mov		ecx, dword [ebp + mySector]
+
+  	inc 	edx				; sector number register
+		mov 	al, cl		; al = lba bits 0-7 
+  	out 	dx, al
+
+  	inc 	edx				; cylinder low register
+  	mov 	al, ch		; al = lba bits 8-15
+  	out 	dx, al
+
+  	inc 	edx				; cylinder high register
+  	ror 	ecx, 16
+  	mov 	al, cl		; set al = lba bits 16-23
+  	out 	dx, al
+
+  	mov		eax, dword [ebp + drive]
+  	inc 	edx				; device/head register
+  	and 	ch, 0Fh		; set ch = lba bits 24-27
+  	shl 	al, 4			; switch device id selection to bit 4
+  	or 		al, 0E0h	; set bit 7 and 5 to 1, with lba = 1
+  	or 		al, ch		; add in lba bits 24-27
+  	out 	dx, al
+
+  	call 	_wait_drdy	; wait for DRDY = 1 (Device ReaDY)
+  	test 	al, 10h		; check DSC bit (Drive Seek Complete)
+  	jz 	short .return_error
+
+  	mov 	al, 20h		; set al = read sector(s) (with retries)
+  	out 	dx, al		; command/status register
+
+  	; TODO: ask for thread yield, giving time for hdd to read data
+  	jmp 	short $+2
+  	jmp 	short $+2
+
+  	call 	ATA_WaitNotBusy.loop			; bypass the "mov edx, 0x1F7"
+  	test 	al, 1     	; check for errors
+  	jnz 	short .return_error
+
+  	mov 	dl, 0F0h	; set dx = 0x1F0 (data register)
+  	mov 	ecx, 256	; 256 words (512 bytes)
+  	repz 	insw		; read the sector to memory
+  	clc			; set completion flag to successful
+  
+  	;popad
+  	ret
+
+.return_error:
+		mov dl, 0xF1		; error status register 0x1F1
+  	in al, dx		; read error code
+  	;popad
+  	mov	eax, -1
+  	ret
+  	
+_wait_drdy:
+; TODO: add check in case DRDY is 0 too long
+		;push	edx
+		mov		edx, dword [ebp + port]
+		add		edx, 7		;status register
+.loop:
+  	in 	al, dx			; read status
+  	test 	al, 40h		; check DRDY bit state
+  	jz 	.loop				; if DRDY = 0, wait
+  	
+  	;pop	edx
+  	ret
+
+ATA_WaitNotBusy:
+; TODO: add error check if drive is busy too long
+		;push	edx
+		mov	edx, dword [ebp + port]
+		add	edx, 7
+.loop:
+  	in 		al, dx
+  	test 	al, 80h
+  	jnz 	.loop
+  	
+  	;pop		edx
+  	ret
+	
+align 4
 ATA_ID_Drive:
-	;%ifdef DEBUG
+	%ifdef DEBUG
 	movzx	eax, byte [ebp + drive]
 	push	eax
 	movzx	eax, word [ebp + port]
@@ -122,7 +262,7 @@ ATA_ID_Drive:
 	lea	eax, [ebp + CheckingATA]
 	push	eax
 	call	[ebp + Print]
-	;%endif
+	%endif
 	
 	mov	edx, 7
 	add	dx, word [ebp + port]
@@ -215,7 +355,9 @@ ATA_ReadData1:
 	;mov	dx,word [port]
         	;mov	al,byte [drive]
 	clc
+
 	call	ATA_ShowInfo
+
 	ret
 	
 ; 0ECh command is for HD drives
@@ -227,7 +369,8 @@ GetATAInfo:
 	call	[ebp + MemAlloc]
 	mov	dword [ebp + ATA_Buffer], eax
 	
-        mov	byte [ebp + command], 0ECh		; Put the hex number for IDENTIFY DRIVE in the var "command".
+  mov	byte [ebp + command], 0ECh		; Put the hex number for IDENTIFY DRIVE in the var "command".
+  ;mov	byte [ebp + command], 0A1h		; Put the hex number for ATAPI IDENTIFY DRIVE in the var "command".
 drive_id:        
 	mov	word [ebp + port], 1F0h			; Try port 1f0 .
 	mov	byte [ebp + drive], 0			; Try master drive 0 (bit 5 should be 0).
@@ -256,3 +399,5 @@ Lets_go:
 	popfd
 	popad
 	ret
+
+
